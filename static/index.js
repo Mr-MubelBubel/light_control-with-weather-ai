83586196270cfde16b94f01fe29f486b362926d2
Maschga <delphi@freenet.de>
2023-05-03 19:01:33 +0200

    changing image-opacity depending on result

--- static/index.js
+++ static/index.js
@@ -1,3 +1,11 @@
+opacities = {
+    "Sonne": 0.0,
+    "Schnee": 0.5,
+    "Nieselregen": 0.7,
+    "Regen": 0.8,
+    "Nebel": 1.0
+}
+
 for (const i of [1, 2, 3, 4]) {
     $("#slider" + i).slider().on('change', function (event) {
         const qs = new URLSearchParams({
@@ -5,28 +13,17 @@ for (const i of [1, 2, 3, 4]) {
             max_temp: document.getElementById("slider2").value,
             min_temp: document.getElementById("slider3").value,
             wind: document.getElementById("slider4").value
-        });
+        })
 
         result = fetch(`http://127.0.0.1:5000/`, {
             method: "POST",
             body: qs
         })
-            .then((response) => response.json())
-            .then(data => {
-                document.getElementById("result").innerHTML = data["result"]
-            })
-            .then(data => {
-                if (data === "Sonne") {
-                    document.getElementById("light").opacity = 0.0
-                } else if (data === "Schnee") {
-                    document.getElementById("light").opacity = 0.5
-                } else if (data === "Regen") {
-                    document.getElementById("light").opacity = 0.8
-                } else if (data === "Nebel") {
-                    document.getElementById("light").opacity = 1.0
-                } else if (data === "Nieselregen") {
-                    document.getElementById("light").opacity = 0.7
-                }
-            });
-    });
+        .then((response) => response.json())
+        .then(data => {
+            document.getElementById("result").innerHTML = data["result"]
+
+            document.getElementById("light").style.opacity = opacities[data["result"]]
+        })
+    })
 }
